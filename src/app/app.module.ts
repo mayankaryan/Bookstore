9ac30c13e7cd02854a5820f85fdecdcfff5d0543
Mayank Aryan <Mayank.Aryan@godigit.com>
2024-08-26 18:07:32 +0530

    ADDED: ui and styling for my cart book display section

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './component/navbar/navbar.component';
+import { MycartComponent } from './component/mycart/mycart.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent
+    NavbarComponent,
+    MycartComponent
   ],
   imports: [
     BrowserModule,
