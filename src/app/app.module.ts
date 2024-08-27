5c08149720c5704e58dc32a68b5d63289ff46539
Mugesh490 <mrmugeshm@gmail.com>
2024-08-27 14:18:33 +0530

    deleted unnecessary component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,11 +8,12 @@ import { ForgotPasswordComponent } from './forgot-password/forgot-password.compo
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { ReactiveFormsModule } from '@angular/forms';
 
+
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
-    ForgotPasswordComponent
+    ForgotPasswordComponent,
   ],
   imports: [
     BrowserModule,
