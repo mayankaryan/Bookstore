1aa82633a76762e6f0bcf5ea7de1bbe6d9e61a25
Rohith Gundu <Rohith.Gundu@godigit.com>
2024-08-27 12:01:17 +0530

    implemented the get book api call

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,29 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './component/navbar/navbar.component';
+import { DashboardComponent } from './component/dashboard/dashboard.component';
+import { BookdetailsComponent } from './component/bookdetails/bookdetails.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MaterialModule } from './moudle/material/material.module';
+import { BookwidgetComponent } from './component/bookwidget/bookwidget.component';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent
+    NavbarComponent,
+    DashboardComponent,
+    BookdetailsComponent,
+    BookwidgetComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MaterialModule,
+    HttpClientModule
+    
+
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/service/http/http.service.ts
+++ src/app/service/http/http.service.ts
@@ -1,9 +1,16 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class HttpService {
+  BasrUrl:string="https://bookstore.incubation.bridgelabz.com/";
+  constructor(private http:HttpClient) { }
+ 
+ GetApiCall(url:string){
+  console.log(this.BasrUrl+url);
+  return this.http.get(this.BasrUrl+url);
 
-  constructor() { }
+ }
 }
