ba425ff8d5cf3c434733b79821b3e999d5b3acf8
Rohith Gundu <Rohith.Gundu@godigit.com>
2024-08-27 12:10:32 +0530

    style.scss merge conflix

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,43 +4,31 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './component/navbar/navbar.component';
-<<<<<<< HEAD
 import { DashboardComponent } from './component/dashboard/dashboard.component';
 import { BookdetailsComponent } from './component/bookdetails/bookdetails.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MaterialModule } from './moudle/material/material.module';
 import { BookwidgetComponent } from './component/bookwidget/bookwidget.component';
 import { HttpClientModule } from '@angular/common/http';
-=======
 import { ForgotPasswordComponent } from './forgot-password/forgot-password.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { ReactiveFormsModule } from '@angular/forms';
->>>>>>> d6c2f2b1ea7966c09f389dfe047562ab619c56d8
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
-<<<<<<< HEAD
     DashboardComponent,
     BookdetailsComponent,
-    BookwidgetComponent
-=======
+    BookwidgetComponent,
     ForgotPasswordComponent
->>>>>>> d6c2f2b1ea7966c09f389dfe047562ab619c56d8
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-<<<<<<< HEAD
     MaterialModule,
-    HttpClientModule
-    
-
-=======
+    HttpClientModule,
     ReactiveFormsModule
->>>>>>> d6c2f2b1ea7966c09f389dfe047562ab619c56d8
   ],
   providers: [],
   bootstrap: [AppComponent]
