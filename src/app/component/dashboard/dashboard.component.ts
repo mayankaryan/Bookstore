c4a4c7fc687780314f98ad5d8c27cc2392a6649d
Rohith Gundu <Rohith.Gundu@godigit.com>
2024-08-28 17:11:43 +0530

    implemented the pagination for the dashboard

--- src/app/component/dashboard/dashboard.component.ts
+++ src/app/component/dashboard/dashboard.component.ts
@@ -24,6 +24,9 @@ export class DashboardComponent {
   booklist:any[]=[];
   @Input() searchText: string = '';
   filteredBookList:any[]=[];
+  currentPage:number=1;
+  itemsPerPage:number=8;
+  totalPages:number=0;
   selectedSortOption: string = 'relevance'; 
   constructor(private httpservice:HttpService,private dataservice:DataService) { 
     this.httpservice.GetApiCall("bookstore_user/get/book").subscribe({
@@ -35,20 +38,17 @@ export class DashboardComponent {
           book_url: this.getRandombookimg()
         }));
         this.filteredBookList=this.booklist;
+        this.updatePagination();
         console.log("filtered",this.booklist);
         console.log(res.result[0]);
       },error:(err:any)=>{
         console.log(err);
       }
     })
-
-   
-    
     this.dataservice.currentMessage.subscribe((message) => {
       this.searchText =message ;
-});
+  });
   }
-
   length(): number {
     return this.booklist.length;
   }
@@ -58,11 +58,41 @@ export class DashboardComponent {
     return this.bookimages[randomIndex];
   }
 
+
+
  filter(event:any){
     this.selectedSortOption=event;
     this.filteredBookList = this.booklist;
     this.filteredBookList.sort((a: any, b: any) => {
       return a[event] - b[event];
     });
+    this.updatePagination();
  }
+
+  updatePagination() {
+    this.totalPages = Math.ceil(this.filteredBookList.length / this.itemsPerPage);
+    this.currentPage = 1;
+  }
+  get paginatedBookList() {
+    const startIndex = (this.currentPage - 1) * this.itemsPerPage;
+    return this.filteredBookList.slice(startIndex, startIndex + this.itemsPerPage);
+  }
+
+    goToPage(page: number) {
+      if (page >= 1 && page <= this.totalPages) {
+        this.currentPage = page;
+      }
+    }
+
+    nextPage() {
+      if (this.currentPage < this.totalPages) {
+        this.currentPage++;
+      }
+    }
+
+    previousPage() {
+      if (this.currentPage > 1) {
+        this.currentPage--;
+      }
+    }
 }
